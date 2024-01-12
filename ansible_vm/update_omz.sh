785f55b1cad2067119b9e36db64b54c77d307484
Ted Chien <hsiangtai.chien@gmail.com>
2024-01-12 12:22:22 +0800

    fix: change update_omz.sh to real file, not symbolic link.

--- ansible_vm/update_omz.sh
+++ ansible_vm/update_omz.sh
@@ -1,1 +1,9 @@
-/home/tedchien/Projects/Vagrant/update_oh_my_zsh.sh
+export OMZ_PATH=$HOME/.oh-my-zsh
+cd $OMZ_PATH/custom/plugins/zsh-autosuggestions
+git pull origin master
+cd $OMZ_PATH/custom/plugins/zsh-completions
+git pull origin master
+cd $OMZ_PATH/custom/plugins/zsh-syntax-highlighting
+git pull origin master
+cd $OMZ_PATH/custom/themes/powerlevel10k
+git pull origin master
